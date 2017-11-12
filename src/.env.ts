d14eaf12f6511a12cd217984d5afa5f628a50fe5
Osama Hassanein <player1os@gmail.com>
2017-11-12 22:40:54 +0100

    Update dependencies

--- src/.env.ts
+++ src/.env.ts
@@ -8,19 +8,20 @@ declare const _env_: {
 	APP_GRACEFUL_SHUTDOWN_TIMEOUT_MS: number, // integer
 	APP_GRACEFUL_SHUTDOWN_HTTP_CONNECTION_TIMEOUT_MS: number, // integer
 
-	APP_CRON_STRING: string,
-	APP_VERIFICATION_THRESHOLD: number, // integer
+	APP_DATABASE_HOST: string,
 	APP_DATABASE_NAME: string,
+	APP_DATABASE_USERNAME: string,
 
 	APP_BACKUP_DIRECTORY: string,
 	APP_BACKUP_EXTENSION: string,
 	APP_BACKUP_MAX_COUNT: number, // integer
 
+	APP_CRON_STRING: string,
+
 	APP_HTTP_IP: string,
 	APP_HTTP_PORT: number, // integer
 
-	APP_DATABASE_HOST: string,
-	APP_DATABASE_USERNAME: string,
+	APP_VERIFICATION_THRESHOLD: number, // integer
 }
 
 export default _env_
